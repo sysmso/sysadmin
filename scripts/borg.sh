97b2a769373797434b6df6125b00c55436d13cd7
Martin Souchal <souchal@apc.in2p3.fr>
2017-06-01 15:49:45 +0200

    New borg conf

--- scripts/borg.sh
+++ scripts/borg.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-REPOSITORY=/mnt/save
+REPOSITORY=ssh://souchal@apcnas2:22/data/portables/comput/apcnb170/save
 #REPOSITORY=/bkp/repo
-/usr/bin/sshfs souchal@apcnis01.in2p3.fr:/portables/comput/apcnb170 /mnt/
 # Backup all of /home and /etc except a few
 # excluded directories
 borg create -v --stats --compression lz4 --progress       \
@@ -12,11 +11,16 @@ borg create -v --stats --compression lz4 --progress       \
     --exclude '/home/*/Videos'                  \
     --exclude '/home/*/Pictures'                \
     --exclude '/home/*/Music'                   \
-    --exclude /home/ssysmso/AUR                 \
+    --exclude '/home/*/docker'                  \
+    --exclude '/home/*/.PlayOnLinux'            \
+    --exclude '/home/ssysmso/AUR'               \
     --exclude '*.avi'                           \
     --exclude '*.mkv'                           \
     --exclude '*.mp4'                           \
+    --exclude '*.MP4'                           \
     --exclude '*.iso'                           \
+    --exclude '*.log'                           \
+    --exclude '*.img'                           \
     --exclude '*.mp3'
 
 # Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
@@ -25,4 +29,3 @@ borg create -v --stats --compression lz4 --progress       \
 # other machine's archives also.
 borg prune -v $REPOSITORY --prefix '{hostname}-' \
     --keep-daily=7 --keep-weekly=4 --keep-monthly=6
-/usr/bin/umount /mnt
